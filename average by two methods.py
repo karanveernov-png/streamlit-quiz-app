feaecd3ed5d87f35e3c31d6e9f683925cfc12a44
Karanveer Singh <ksnaman2006@gmail.com>
2026-06-19 14:57:01 +0530

    Fix requirements for cloud deployment

--- average by two methods.py
+++
@@ -1,6 +0,0 @@
-a=float(input("enter 1st no:"))
-b=float(input("enter 2nd no:"))
-average=(a+b)/2
-print("the average of above both noes are",average)
-average2=sum([a,b])/len([a,b])
-print("the average of above both noes are",average2)
